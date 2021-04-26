4e77025c57838265d6d970b972165f557c55bc8f
Sanskar Bansal <sanskarbansal@Sanskars-MacBook-Air.local>
2021-04-26 21:32:52 +0530

    Add event listeners on 3 buttons

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 var btns = document.getElementsByClassName("btn");
 var pspeed = document.getElementById("speed");
 var currentSpeed = document.querySelector("#currentSpeed span");
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 19; i++) {
     btns[i].addEventListener("click", (event) => {
         chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, event.target.id);
